21cb11afa8d7c1de37bb6bd31bdfd255babcf76e
Chintan Patel <chintanpatelit024@gmail.com>
2025-05-01 14:06:59 +0530

    Added: Implement Quick Sort

--- Sorting-Stick/include/Gameplay/Collection/StickCollectionController.h
+++ Sorting-Stick/include/Gameplay/Collection/StickCollectionController.h
@@ -47,12 +47,16 @@ namespace Gameplay
             void merge(int left, int mid, int right);
             void mergeSort(int left, int right);
 
+            int partition(int left, int right);
+            void quickSort(int left, int right);
+
             void processSortThreadState();
 
             void processBubbleSort();
             void processInsertionSort();
             void processSelectionSort();
             void processMergeSort();
+            void processQuickSort();
 
             void inPlaceMerge(int left, int mid, int right);
             void inPlaceMergeSort(int left, int right);
